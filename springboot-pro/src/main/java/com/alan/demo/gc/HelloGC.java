9c6e9eae6db0f974fff3a753a3acd65c9f87c71a
gaodehan <573308611@qq.com>
2020-01-31 18:38:42 +0800

    盘点JVM家底参数

--- springboot-pro/src/main/java/com/alan/demo/gc/HelloGC.java
+++ springboot-pro/src/main/java/com/alan/demo/gc/HelloGC.java
@@ -5,6 +5,19 @@ package com.alan.demo.gc;
  * 调试相关命令
  * 1.jps -l
  * 2. jinfo -flag MetaspaceSize 线程ID
+ * <p>
+ * 第一种,查看参数盘点点家底
+ * jps
+ * jinfo -flag 具体参数  java进程编号
+ * jinfo -flags         java进程编号
+ * <p>
+ * 第二种,查看参数盘点家底
+ * java -XX:+PrintFlagsInitial   查看JVM初始化的一些参数
+ * java -XX:+PrintFlagsFinal = :=    主要查看修改更新
+ * java -XX:+PrintFlagsFinal -XX:MetaspaceSize=512 T 其中T为类的名字   修改初始化参数的值并同时查看
+ *
+ * 第三种,查看参数盘点家底
+ * java -XX:+PrintCommandLineFlags -version
  * @Author gaodehan
  * @Version V1.0.0
  * @Since 1.0
