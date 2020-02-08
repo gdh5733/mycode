e1f99c2ee76006524dd19ab171f78ff63f1fbebf
gaodehan <573308611@qq.com>
2020-02-08 14:51:54 +0800

    redis 解决分布式session 混乱问题 jar准备

--- springboot-pro/src/main/java/com/alan/demo/config/RedisConfig.java
+++ springboot-pro/src/main/java/com/alan/demo/config/RedisConfig.java
@@ -2,15 +2,10 @@ package com.alan.demo.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
