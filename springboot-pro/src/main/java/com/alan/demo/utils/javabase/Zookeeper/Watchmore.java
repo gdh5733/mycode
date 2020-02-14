ca5ad525cc89b215f0675efd1bcd0b0f1c39873b
gaodehan <573308611@qq.com>
2020-02-14 21:10:08 +0800

    watchMore

--- springboot-pro/src/main/java/com/alan/demo/utils/javabase/Zookeeper/Watchmore.java
+++ springboot-pro/src/main/java/com/alan/demo/utils/javabase/Zookeeper/Watchmore.java
@@ -8,7 +8,7 @@ import org.apache.zookeeper.data.Stat;
 import java.io.IOException;
 
 /**
- * @Description Watchone
+ * @Description Watchmore
  * ZooKeeper 支持watch(观察)的概念。客户端可以在每个znode结点上设置一个观察。
  * 如果被观察服务端的znode结点有变更,那么watch就会被触发,
  * 这个watch所属的客户端将收到一个通知包被告知结点已经发生变化,把相应的事件通知给设置Watcher的Client端
@@ -22,11 +22,14 @@ import java.io.IOException;
 
 
 @Data
-public class Watchone extends BaseConfig {
+public class Watchmore extends BaseConfig {
 
-    private static final Logger logger = Logger.getLogger(Watchone.class);
+    //    private static final Logger logger = Logger.getLogger(Watchmore.class);
     //实例变量
     private ZooKeeper zk = null;
+
+    private String oldValue = null;
+
 
     /**
      * 初始化 相当于拆创建了一个跟ZooKeeper服务器的连接
@@ -81,6 +84,7 @@ public class Watchone extends BaseConfig {
             }
         }, new Stat());
         result = new String(byteArray);
+        oldValue = result;
         return result;
     }
 
@@ -89,30 +93,48 @@ public class Watchone extends BaseConfig {
      *
      * @return
      */
-    private String trigerValue(String PATH) throws KeeperException, InterruptedException {
+    private boolean trigerValue(String PATH) throws KeeperException, InterruptedException {
         String result = null;
-        byte[] byteArray = zk.getData(PATH, false, new Stat());
+        byte[] byteArray = zk.getData(PATH, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                try {
+                    //一个递归
+                    trigerValue(PATH);
+                } catch (KeeperException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Stat());
         result = new String(byteArray);
-        System.out.println("***************watch one time :" + result);
-//        logger.info("***************watch one time :" + result);
 
-        return result;
+        String newValue = result;
 
+        if (oldValue.equals(newValue)) {
+            System.out.println("*************no changes**********");
+//            logger.info();
+            return false;
+        } else {
+            System.out.println("**************oldValue: " + oldValue + "\t newValue: " + newValue);
+//            logger.info("**************oldValue: " + oldValue + "\t newValue: " + newValue);
+            oldValue = newValue;
+            return true;
+        }
     }
 
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
-        Watchone watchone = new Watchone();
-        watchone.setZk(watchone.startZk());
+        Watchmore watchMore = new Watchmore();
+        watchMore.setZk(watchMore.startZk());
 
-        if (watchone.getZk().exists(PATH, false) == null) {
-            watchone.createZnode(PATH, "AAA");
-            String retValue = watchone.getZnode(PATH);
+        if (watchMore.getZk().exists(PATH, false) == null) {
+            watchMore.createZnode(PATH, "AAA");
+            String retValue = watchMore.getZnode(PATH);
             System.out.println("******************* first retValue: " + retValue);
 //            logger.info("******************* first retValue: " + retValue);
             Thread.sleep(Long.MAX_VALUE);
         } else {
-            System.out.println("************node ok");
+            System.out.println("************node ok  " + watchMore.getZnode(PATH));
 //            logger.info("************node ok");
         }
     }
