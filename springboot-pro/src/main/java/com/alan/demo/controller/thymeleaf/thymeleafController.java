2127e7ea93064186030aa3856a897cdccfd5f0e8
gaodehan <573308611@qq.com>
2020-02-21 12:20:28 +0800

    thymeleaf集成

--- springboot-pro/src/main/java/com/alan/demo/controller/thymeleaf/thymeleafController.java
+++ springboot-pro/src/main/java/com/alan/demo/controller/thymeleaf/thymeleafController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import java.util.Map;
  */
 @Api(description = "测试thymeleaf接口")
 @RequestMapping("/thy")
-@RestController
+@Controller
 @Slf4j
 public class thymeleafController {
 
