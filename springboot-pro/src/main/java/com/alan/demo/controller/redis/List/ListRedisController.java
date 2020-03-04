f9411523744d168c3590e96709020ea0f6b3bc04
gaodehan <573308611@qq.com>
2020-03-04 21:21:52 +0800

    提交代码

--- springboot-pro/src/main/java/com/alan/demo/controller/redis/List/ListRedisController.java
+++ springboot-pro/src/main/java/com/alan/demo/controller/redis/List/ListRedisController.java
@@ -1,5 +1,4 @@
 package com.alan.demo.controller.redis.List;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
